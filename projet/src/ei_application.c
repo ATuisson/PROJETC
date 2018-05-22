c84d3dd77936d4ce8aa6a09a843ceb77f086d0b1
Basile Simonin <Basile.Simonin@grenoble-inp.org>
2018-05-22 15:02:15 +0200

    ecriture de la fonction recursive pour parcourir l'arbre

--- projet/src/ei_application.c
+++ projet/src/ei_application.c
@@ -2,24 +2,38 @@
 #include "hw_interface.h"
 #include <stdio.h>
 
+void explore(ei_widget_t* noeud);
 void ei_app_create(ei_size_t* main_window_size, ei_bool_t fullscreen)
 {
 
-  hw_init();
-  // il va falloir appeler ei_widgetclass_register ici.
-//  surface_fenetre_syst = malloc(main_window_size->width*main_window_size->height*4)
-  ei_surface_t surface_fenetre_syst = NULL;
-  surface_fenetre_syst = hw_create_window(main_window_size, fullscreen);
+        hw_init();
+        ei_frame_register_class();
+        // il va falloir appeler ei_widgetclass_register ici.
+
+      //  surface_fenetre_syst = malloc(main_window_size->width*main_window_size->height*4)
+        ei_surface_t surface_fenetre_syst = NULL;
+        surface_fenetre_syst = hw_create_window(main_window_size, fullscreen);
 }
 
 
 void ei_app_free(void)
 {
+
         hw_quit();
 }
 
 void ei_app_run(void)
-{
+{       ei_widget_t*  racine=;
+        ei_widget_t*  courant=;
+        while (courant.children_head != NULL) && (courant.next_sibling != NULL){
+
+        }
+        // partir de la racine et "recursivement" passer les desendants et appeler leur drawfunc
+        // racine : faire une variable globale qui memorise la racine ?
+        // tant que children != null continuer
+        // sinon voir next sibling et recommencer la partie d'avant
+        // next sibling = null : voir parent next sibling si nul voir parent next sibling etc
+
         getchar();
 }
 
@@ -40,5 +54,18 @@ ei_widget_t* ei_app_root_widget(void)
 
 ei_surface_t ei_app_root_surface(void)
 {
+
+}
+void explore(ei_widget_t* noeud)
+{
+        draw(noeud);
+        if (noeud.children_tail != NULL){
+                explore(noeud.children_tail);
+                ei_widget_t* current = noeud.children_tail.next_sibling;
+                while (current != NULL){
+                      explore(current);
+                      current = current.next_sibling;
+                }
+        }
 
 }
