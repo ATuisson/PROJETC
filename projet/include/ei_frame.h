e6bf5d41b1a4e118036d2ef010a1b85fb6da1109
Lucas.et <lucas.etourneau1@outlook.fr>
2018-05-22 16:45:26 +0200

    ROOT widget

--- projet/include/ei_frame.h
+++ projet/include/ei_frame.h
@@ -18,7 +18,7 @@
  * \brief A structure that stores specific information about a frame widget class
  */
 typedef struct ei_frame_t {
-        ei_widget_t*          widget; ///< Structure commune enntre tous les widgets
+        ei_widget_t*          widget; ///< Structure commune entre tous les widgets
         const ei_color_t*     color; ///< Couleur du cadre
         int*                  border_width; ///< largeur de la bordure du cadre
         ei_relief_t*          relief; ///< Relief du cadre
--- projet/src/ei_application.c
+++ projet/src/ei_application.c
@@ -1,16 +1,20 @@
 #include "ei_application.h"
 #include "hw_interface.h"
+#include "ei_widgetclass.h"
+#include "ei_types.h"
 #include <stdio.h>
 
-void explore(ei_widget_t* noeud);
+ei_widget_t* ROOT = NULL;
+
 void ei_app_create(ei_size_t* main_window_size, ei_bool_t fullscreen)
 {
 
         hw_init();
+  // il va falloir appeler ei_widgetclass_register ici.
+//  surface_fenetre_syst = malloc(main_window_size->width*main_window_size->height*4)
         ei_frame_register_class();
-        // il va falloir appeler ei_widgetclass_register ici.
-
-      //  surface_fenetre_syst = malloc(main_window_size->width*main_window_size->height*4)
+        ei_color_t root_color = {20,20,20,255};
+        *ROOT = {CLASSES, 1, root_color, NULL, NULL, NULL, NULL, NULL};
         ei_surface_t surface_fenetre_syst = NULL;
         surface_fenetre_syst = hw_create_window(main_window_size, fullscreen);
 }
@@ -18,22 +22,11 @@ void ei_app_create(ei_size_t* main_window_size, ei_bool_t fullscreen)
 
 void ei_app_free(void)
 {
-
         hw_quit();
 }
 
 void ei_app_run(void)
-{       ei_widget_t*  racine=;
-        ei_widget_t*  courant=;
-        while (courant.children_head != NULL) && (courant.next_sibling != NULL){
-
-        }
-        // partir de la racine et "recursivement" passer les desendants et appeler leur drawfunc
-        // racine : faire une variable globale qui memorise la racine ?
-        // tant que children != null continuer
-        // sinon voir next sibling et recommencer la partie d'avant
-        // next sibling = null : voir parent next sibling si nul voir parent next sibling etc
-
+{
         getchar();
 }
 
@@ -54,18 +47,5 @@ ei_widget_t* ei_app_root_widget(void)
 
 ei_surface_t ei_app_root_surface(void)
 {
-
-}
-void explore(ei_widget_t* noeud)
-{
-        draw(noeud);
-        if (noeud.children_tail != NULL){
-                explore(noeud.children_tail);
-                ei_widget_t* current = noeud.children_tail.next_sibling;
-                while (current != NULL){
-                      explore(current);
-                      current = current.next_sibling;
-                }
-        }
 
 }
