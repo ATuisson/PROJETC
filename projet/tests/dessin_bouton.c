255e8bd05f5d2a15eb4edcecd7becddcff6acb4a
Basile Simonin <Basile.Simonin@grenoble-inp.org>
2018-05-29 12:15:26 +0200

    draw_button avec un petit soucis de {}

--- projet/tests/dessin_bouton.c
+++ projet/tests/dessin_bouton.c
@@ -12,7 +12,9 @@ int ei_main(int argc, char** argv)
 	ei_surface_t			main_window		= NULL;
 	ei_size_t			main_window_size;
 	ei_color_t			red			= { 0xff, 0x00, 0x00, 0xff };
-	ei_color_t			transp_blue		= { 0x00, 0x00, 0xff, 0x88 };
+	ei_color_t      transp_blue1  = {0x00, 0x00, 0xff, 0x88 };
+	ei_color_t			transp_blue2	= {0x00, 0x00, 0xaa, 0x88 };
+	ei_color_t      blue  = {0x00, 0x00, 0xbf, 0xff};
 
 	// Init acces to hardware.
 	hw_init();
@@ -26,17 +28,26 @@ int ei_main(int argc, char** argv)
 
 	// Fill the main window in red, draw a transparent blue polygon:
 	// PARAM DU BOUTON
-	ei_bouton_param_t type_bouton = ei_bouton_bot;
+	ei_bouton_param_t type_bouton1 = ei_bouton_top;
+	ei_bouton_param_t type_bouton2 = ei_bouton_bot;
+	ei_bouton_param_t type_bouton3 = ei_bouton_whole;
 	// Define the polygon vertices.
   ei_point_t point_du_rectangle = {50, 50};
   ei_size_t taille_rectangle = {300, 200};
-  ei_rect_t rectangle = {point_du_rectangle, taille_rectangle};
-  ei_linked_point_t* points = rounded_frame(rectangle, 80,type_bouton);
-
+	ei_rect_t rectangle = {point_du_rectangle, taille_rectangle};
+	// border de 10
+	ei_point_t point_du_rectangle_sans_border = {60,60};
+	ei_size_t taille_rectangle_sans_border = {280,180};
+	ei_rect_t rectangle_sans_border = {point_du_rectangle_sans_border, taille_rectangle_sans_border};
+  ei_linked_point_t* points1 = rounded_frame(rectangle, 30,type_bouton1);
+	ei_linked_point_t* points2 = rounded_frame(rectangle, 30,type_bouton2);
+	ei_linked_point_t* points3 = rounded_frame(rectangle_sans_border, 20,type_bouton3);
 	// Lock the surface for drawing, fill, draw polygon, unlock, update screen.
 	hw_surface_lock(main_window);
 	ei_fill(main_window, &red, NULL);
-	ei_draw_polygon(main_window, points, transp_blue, NULL);
+	ei_draw_polygon(main_window, points1, transp_blue1, NULL);
+	ei_draw_polygon(main_window, points2, transp_blue2, NULL);
+	ei_draw_polygon(main_window, points3, blue, NULL);
 	hw_surface_unlock(main_window);
 	hw_surface_update_rects(main_window, NULL);
 	getchar();
